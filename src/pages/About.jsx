2fbc9299258c90b86e3e706fdf0306e3dbd62027
Awez  Sheikh <91287127+Awezsk@users.noreply.github.com>
2025-10-24 13:01:36 +0530

    Revise job title and interests in About page
    Updated job title and added 'automation' to interests.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,8 +19,8 @@ const About = () => {
       </div>
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p>
-          I'm an Web developer with Intrest in AI ML from India. I'm passionate about data science and
-          machine learning.
+          I'm an IT Specialist with Intrest in AI ML from India. I'm passionate about data science and
+          machine learning and automation.
         </p>
       </div>
       <div className="py-10 flex flex-col ">
